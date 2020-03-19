63db241e91c7fd7f4c9825eaca08a0ed394a2927
thefenriswolf <stefan.rohrbacher97@gmail.com>
2020-03-19 20:12:21 +0100

    cleanup

--- shell.nix
+++ shell.nix
@@ -1,16 +1,12 @@
-with import <nixpkgs> {};
-mkShell {
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.mkShell {
   buildInputs = [
-	(python3.withPackages (ps: with ps; [
-	pydicom
-	python38Packages.matplotlib
-	numpy
-	python38Packages.pillow
-	pandas
-	python38Packages.opencv4
-	]))
-    nano
-    emacs
-    git
+    pkgs.nano
+    pkgs.tree
+    pkgs.rsync
+    pkgs.imagemagick
+    pkgs.detox
   ];
 }
